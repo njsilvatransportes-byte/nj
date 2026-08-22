84c845575191ccc44de8963e7d3de87e85abc88c
njsilvatransportes-byte <njsilva.transportes@gmail.com>
2026-08-22 19:43:16 -0300

    Add files via upload

--- timeout.js
+++ timeout.js
@@ -3,11 +3,11 @@
   function resetTimer() {
     clearTimeout(inactivityTimer);
     inactivityTimer = setTimeout(() => {
-      localStorage.removeItem('nj_session');
+      sessionStorage.removeItem('nj_session');
       if (window.location.pathname.endsWith('index.html') || window.location.pathname === '/' || window.location.pathname === '') {
         location.reload();
       } else {
-        window.location.href = 'index.html';
+        window.location.href = 'dashboard.html';
       }
     }, 30 * 60 * 1000); // 30 minutes
   }
